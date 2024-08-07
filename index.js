6bd0c0498cbbb7b704ef040c0c8c1c8558630dc8
Mohamed Mahmoud Bady <mohamedmahmoudbady@Mohameds-MBP.lan>
2024-08-07 15:21:28 -0400

    add the modification

--- index.js
+++ index.js
@@ -35,46 +35,46 @@
 //part 3
 //const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-const csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+//const csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+const csv="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+// let output = "";
 
-let output = "";
-
-for(let i = 0; i < csv.length; i++) {
-  if (csv[i] === '\r') {
-    i++;
-  } else if (csv[i] === '\n') {
-    let cell1, cell2, cell3, cell4;
-    let current_cell = "";
-    let counter = 1;
-    for(let j = 0; j < output.length; j++) {
-      if (output[j] === ",") {
-        switch(counter){
-          case 1:
-            cell1 = current_cell;
-            break;
-          case 2:
-            cell2 = current_cell;
-            break;
-          case 3:
-            cell3 = current_cell;
-            break;
-          default:
-            break;
-        }
-        counter++;
-        current_cell = "";
-      } else {
-        current_cell += output[j];
-      }
-    }
-    cell4 = current_cell;
-    console.log(cell1, cell2, cell3, cell4);
-    output = "";
-    i++;
-  } else {
-    output += csv[i];
-  }
-}
+// for(let i = 0; i < csv.length; i++) {
+//   if (csv[i] === '\r') {
+//     i++;
+//   } else if (csv[i] === '\n') {
+//     let cell1, cell2, cell3, cell4;
+//     let current_cell = "";
+//     let counter = 1;
+//     for(let j = 0; j < output.length; j++) {
+//       if (output[j] === ",") {
+//         switch(counter){
+//           case 1:
+//             cell1 = current_cell;
+//             break;
+//           case 2:
+//             cell2 = current_cell;
+//             break;
+//           case 3:
+//             cell3 = current_cell;
+//             break;
+//           default:
+//             break;
+//         }
+//         counter++;
+//         current_cell = "";
+//       } else {
+//         current_cell += output[j];
+//       }
+//     }
+//     cell4 = current_cell;
+//     console.log(cell1, cell2, cell3, cell4);
+//     output = "";
+//     i++;
+//   } else {
+//     output += csv[i];
+//   }
+// }
 // const rows = csv.split("\n");
 // for(let row of rows) {
 //     let cell1, cell2, cell3, cell4;
@@ -111,4 +111,34 @@ for (let row of rows) {
     }
     console.log(cell1, cell2, cell3, cell4);
 }
+//part 3
+const ks=rows[0].split(",").map(ks =>ks.toLowerCase());
+const dataobject=[]
+for(let i=1;i<rows.length;i++){
+    const valobj=rows[i].split(",");
+    const obj={};
+    for(let j=0;j<ks.length;j++){
+        obj[ks[j]]=valobj[j];
+    }
+    dataobject.push(obj);
+}
+console.log(dataobject);
+
+//part 4
+dataobject.pop();
+dataobject.splice(1,0,{ id: "48", name: "Barry", occupation: "Runner", age: "25" });
+dataobject.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+let ages=0;
+for(let obj of dataobject){
+    ages+= parseInt(obj.age);
+}
+let avg=ages/dataobject.length;
+console.log("The average age of the group is :",avg);
+
+//part 5
+
+const id=Object.keys(dataobject[0]).join(",");
+const details=dataobject.map(obj =>Object.values(obj).join(","));
+const csv_2=[id,...details].join("\n");
+console.log(csv_2);
 
